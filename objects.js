c68b4ccc8c31ac8b01825207d9211f1a3a234148
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-05 22:39:17 +0600

    object sum

---
+++ objects.js
@@ -0,0 +1,31 @@
+// //Objects are variable too but it can contain many values.
+// const car = {
+//     brand: "Tesla",
+//     model: 3,
+//     weight: "9980kg",
+//     seating: "5 Adult",
+//     color: "red"
+
+// }
+// // console.log(car.color);
+// // console.log(car['brand']);
+// let property = car;
+// console.log(property);
+
+const a = {
+    name: "laptop",
+    color: "black",
+    price: 12000
+}
+const b = {
+    name: "monitor",
+    color: "black",
+    price: 5000
+}
+const c = {
+    name: "mobile",
+    color: "black",
+    price: 2500
+}
+const totalPrice = a.price + b.price + c.price;
+console.log(totalPrice);
