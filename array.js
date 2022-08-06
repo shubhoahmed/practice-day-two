6b2f6fabde377c38d2e8423ae03bd6f8f3cb9542
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-06 12:40:37 +0600

    array jala jala

--- array.js
+++ array.js
@@ -9,5 +9,23 @@ let index = friendAge.indexOf(25);
 //add element to last 
 friendAge.push(64);
 friendAge.push(40);
+//remove element from array
+friendAge.pop();
 
-console.log(lengthOfArray);
+const ageNextYear = [];
+// ageNextYear.push(element);
+// console.log(ageNextYear);
+
+let sum = 0;
+for (let i = 0; i < friendAge.length; i++) {
+    let index = i;
+    let arrEle = friendAge[index];
+    if (arrEle % 2 == 1) {
+        ageNextYear.push(arrEle);
+        sum = sum + arrEle;
+    }
+}
+
+
+console.log(ageNextYear);
+console.log(sum);
