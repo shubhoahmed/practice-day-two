19be5c69c3a02d85710438da4ac9b621b1317df5
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-06 11:49:43 +0600

    array practice

---
+++ array.js
@@ -0,0 +1,13 @@
+// Declared an array 
+let friendAge = [24, 23, 21, 26, 25, 27, 20, 19];
+//find length of an array
+let lengthOfArray = friendAge.length;
+//get element or value by index
+let element = friendAge[2];
+//get index by element or value
+let index = friendAge.indexOf(25);
+//add element to last 
+friendAge.push(64);
+friendAge.push(40);
+
+console.log(lengthOfArray);
--- function.js
+++ function.js
@@ -10,7 +10,3 @@ function odd_even(n) {
 const number = odd_even(46);
 console.log('This is an ', number);
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let element = arr[4];
-let index = arr.indexOf(9);
-console.log(arr, 'Element: ', element, 'This is an index: ', index);
